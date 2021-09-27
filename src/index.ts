61ebc30f3f2f78719444d63ad2c339085dd6f338
Andrew Hall <andrew@rioja.io>
2021-09-27 15:42:47 +0100

    feat: export event emitter type

--- src/index.ts
+++ src/index.ts
@@ -2,32 +2,47 @@ export type EventMap = {
   [k: string]: object | undefined;
 };
 
+export interface EventEmitter<T extends EventMap> {
+  on: <K extends keyof T>(eventType: K, handler: EventHandler<T, K>) => number;
+  off: <K extends keyof T>(eventType: K, id: number) => void;
+  once: <K extends keyof T>(eventType: K, handler: EventHandler<T, K>) => void;
+  emit: <K extends keyof T>(
+    eventType: keyof T,
+    ...params: OptionalArg<K>
+  ) => void;
+}
+
 type OptionalArg<T> = T extends undefined ? [] : [T];
 
-export type EventEmitter = ReturnType<typeof createEventEmitter>
+type EventType<T extends EventMap> = keyof T;
+type Event<T extends EventMap, K extends EventType<T>> = T[K];
+type ListenerStore<T extends EventMap> = {
+  [K in EventType<T>]: ListenerEntries<T, K>;
+};
+type ListenerEntries<T extends EventMap, K extends EventType<T>> = {
+  [key: string]: Listener<T, K>;
+};
+
+interface Listener<T extends EventMap, K extends EventType<T>> {
+  id: number;
+  handler: EventHandler<T, K>;
+}
+type EventHandler<T extends EventMap, K extends EventType<T>> = (
+  ...params: OptionalArg<Event<T, K>>
+) => void;
 
 export function createEventEmitter<T extends EventMap>() {
-  type EventType = keyof T;
-  type Event<K extends EventType> = T[K];
-  type ListenerStore = { [K in EventType]: ListenerEntries<K> };
-  type ListenerEntries<K extends EventType> = { [key: string]: Listener<K> };
-
-  interface Listener<K extends EventType> {
-    id: number;
-    handler: EventHandler<K>;
-  }
-  type EventHandler<K extends EventType> = (
-    ...params: OptionalArg<Event<K>>
-  ) => void;
-
-  const listeners = {} as ListenerStore;
+  const listeners = {} as ListenerStore<T>;
   let idSeq = 0;
 
-  function on<K extends EventType>(eventType: K, handler: EventHandler<K>) {
+  function on<K extends EventType<T>>(
+    eventType: K,
+    handler: EventHandler<T, K>
+  ) {
     const id = idSeq++;
 
     listeners[eventType] = listeners[eventType] || {};
-    (listeners[eventType] as ListenerEntries<K>)[id.toString()] = {
+    (listeners[eventType] as ListenerEntries<T, K>)[id.toString()] = {
       id,
       handler,
     };
@@ -35,8 +50,8 @@ export function createEventEmitter<T extends EventMap>() {
     return id;
   }
 
-  function off<K extends EventType>(eventType: K, id: number) {
-    const eventListeners: ListenerEntries<K> = listeners[eventType];
+  function off<K extends EventType<T>>(eventType: K, id: number) {
+    const eventListeners: ListenerEntries<T, K> = listeners[eventType];
     const idx = id.toString();
 
     if (eventListeners && eventListeners[idx]) {
@@ -46,7 +61,10 @@ export function createEventEmitter<T extends EventMap>() {
     }
   }
 
-  function once<K extends EventType>(eventType: K, handler: EventHandler<K>) {
+  function once<K extends EventType<T>>(
+    eventType: K,
+    handler: EventHandler<T, K>
+  ) {
     let id = -1;
     id = on(eventType, (...args: Parameters<typeof handler>) => {
       handler(...args);
@@ -54,9 +72,9 @@ export function createEventEmitter<T extends EventMap>() {
     });
   }
 
-  function emit<K extends EventType>(
-    eventType: EventType,
-    ...params: OptionalArg<Event<K>>
+  function emit<K extends EventType<T>>(
+    eventType: EventType<T>,
+    ...params: OptionalArg<Event<T, K>>
   ) {
     const eventListeners = listeners[eventType];
 
